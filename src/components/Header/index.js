8b5cc66b27603acb1631e57c89330d3df47ad99a
betteriq <betteriq@yahoo.com>
2022-09-16 11:09:40 +0430

    master

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./index.css"
+const index = () => {
+    return (
+        <div className='hero'>
+            <div className="left-side">left side</div>
+            <div className="right-side">right side</div>
+        </div>
+    )
+}
+
+export default index
