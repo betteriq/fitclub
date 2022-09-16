4fa21f7737d28108baad9a0affb9e1c250ebf7c5
betteriq <betteriq@yahoo.com>
2022-09-16 11:57:23 +0430

    first

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import "./index.css"
+import Logo from "../../assets/logo.png"
 const index = () => {
     return (
         <div className='hero'>
-            <div className="left-side">left side</div>
+            <div className="left-side">
+                <div className="header-left">
+                    <img src={Logo} alt="" className='logo' />
+                    <ul>
+                        <li>Home</li>
+                        <li>Programs</li>
+                        <li>Why us</li>
+                        <li>Plans</li>
+                        <li>Testimonials</li>
+                    </ul>
+                </div>
+                <span className='button-left'>The best fitness club in the town</span>
+                <div className='st'>
+                    <span className='stroke-text'>Shape</span>
+                    <span>your</span>
+                </div>
+                <div>
+                    <span>Ideal body</span>
+                </div>
+            </div>
             <div className="right-side">right side</div>
         </div>
     )
