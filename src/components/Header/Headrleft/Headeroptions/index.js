d80245c7cf8a078c29d90a4145b098386f8c794f
betteriq <betteriq@yahoo.com>
2022-09-17 18:07:36 +0430

    second

---
+++ src/components/Header/Headrleft/Headeroptions/index.js
@@ -0,0 +1,25 @@
+import "./index.css"
+
+
+const index = () => {
+    return (
+        <>
+            <div className="header-options">
+                <div>
+                    <span>+ 140</span>
+                    <span>Expert coach</span>
+                </div>
+                <div>
+                    <span>+ 978</span>
+                    <span>members joined</span>
+                </div>
+                <div>
+                    <span>+ 50</span>
+                    <span>fitness program</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default index
